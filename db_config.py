a0d609b99c3944665a51f3bf97ae2eed8c87027c
rsato <sato.ryuta.main@gmail.com>
2022-04-25 04:43:00 +0900

    :lock: refactor(db_config): Remove PASSWORD
    BREAKING CHANGE: Must be set PostgreSQL PASSWORD to environment variable

--- db_config.py
+++ db_config.py
@@ -1,5 +1,7 @@
+import os
+
 from postgresql_connect import PostgreConnect
 def db_config():
-    pg = PostgreConnect(password='Q2w3oert192837', )
+    pg = PostgreConnect(password=os.environ.get('SECURITY_KEY'))
     print(f'connected {pg.get_table_list()}')
 
