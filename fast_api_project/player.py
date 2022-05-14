0ae4e6c8d659db7533b014fa82b8eeafa2ea3784
rsato <sato.ryuta.main@gmail.com>
2022-05-15 00:08:13 +0900

    :truck: style(Player): , Renamed ambiguous names
    Player.put_out() -> Player.get_card_from_client_to_board()
    Player.returned_invalid_card() -> Player.returned_invalid_card_from_board_to_player()

--- fast_api_project/player.py
+++ fast_api_project/player.py
@@ -11,8 +11,11 @@ class Player:
     def get_ulid(self):
         return self.__ulid
 
-    def put_out(self) -> Card:
+    def get_card_from_client_to_board(self) -> Card:
+        # Clientから受け取る．
+        # 整合性を調べる
+        # 値を盤面に返す
         return self.cards.pop()
 
-    def returned_invalid_card(self, card):
+    def returned_invalid_card_from_board_to_player(self, card):
         self.cards.append(card)
