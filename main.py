632aa9f15f5bb0d0b57bb5e38fc1dc7081b05580
rsato <sato.ryuta.main@gmail.com>
2022-06-12 00:33:15 +0900

    :recycle: refactor(User(BaseModel)): User class is added and UserIn class and UserOut class also added for receive model and response model

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from ws_manage import Manager, WebSocket
-from user import User
-from fastapi import FastAPI, WebSocketDisconnect, Depends
+from user import UserIn, UserOut
+from fastapi import FastAPI, WebSocketDisconnect, Depends, Form
 from fastapi.security import OAuth2PasswordBearer
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse  # , ORJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from db_config import db_config
 import json
+
 db_config()
-app = FastAPI(version="0.75.1")
+app = FastAPI(
+    debug=True,
+    version="0.75.1",
+    # default_response_class=ORJSONResponse
+)
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 manager = Manager()
 
 
@@ -34,23 +52,41 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
     try:
         while True:
-            data = await websocket.receive_text()
+            received_msg = await websocket.receive_text()
+            json_msg = json.loads(received_msg)
+            print(json_msg)
             # data = await websocket.receive_json()
             reply_dict = {
                 "client": websocket.headers.get('sec-websocket-key'),
-                "status": "online",
-                "message": data
+                "status": json_msg["status"],
+                "date": json_msg["date"],
+                "message": json_msg["message"]
             }
             # for key in websocket.headers.keys():
             #     print(key, websocket.headers.get(key))
-            print(f"client:{websocket.headers.get('sec-websocket-key')} {websocket.client}")
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            print("client:{} {}".format(
+                websocket.headers.get('sec-websocket-key'),
+                json_msg['message']
+            ))
+            # await manager.send_personal_message(f"You wrote: {json_msg['message']}", websocket)
             # await manager.broadcast(f"Client #{client_id} says: {data}")
             await manager.broadcast(json.dumps(reply_dict))
-            await manager.send_personal_message(f"Matching cueue.. in {len(manager.active_connections)}", websocket)
+            reply_dict = {
+                "client": websocket.headers.get('sec-websocket-key'),
+                "status": "connecting",
+                "date": json_msg["date"],
+                "message": f"Matching cueue.. in {len(manager.active_connections)}"
+            }
+            await manager.send_personal_message(json.dumps(reply_dict), websocket)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        reply_dict = {
+            "client": websocket.headers.get('sec-websocket-key'),
+            "status": "disconnected",
+            "message": "Client #{} left the chat".format(client_id)
+        }
+        await manager.broadcast(json.dumps(reply_dict))
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -58,3 +94,17 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 @app.post("/items/")
 async def read_items(token: str = Depends(oauth2_scheme)):
     return {"token": token}
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user: UserIn):
+    # データベースにアカウントを追加する
+    print(user.json(), "by post")
+
+    return user
+
+
+@app.post("/login/")
+async def login(username: str = Form(None), password: str = Form(None)):
+    print(username, password)
+    return {"username": username}
--- user.py
+++ user.py
@@ -1,7 +1,23 @@
 from fastapi import WebSocket
+from pydantic import BaseModel # , EmailStr
 
 
-class User:
-    def __init__(self, websocket: WebSocket, name):
+class User(BaseModel):
+
+    def __init__(self, websocket: WebSocket, name, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.ws = websocket
         self.name = name
+
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    # email: EmailStr
+    full_name: str | None = None
+
+
+class UserOut(BaseModel):
+    username: str
+    # email: EmailStr
+    full_name: str | None = None
