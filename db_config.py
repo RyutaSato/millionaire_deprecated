545b88da5f368c81f545fc24040b136be410db12
rsato <sato.ryuta.main@gmail.com>
2022-08-27 03:43:00 +0900

    :hammer: Refactored database engine code. the before code is duplicated.

--- db_config.py
+++ db_config.py
@@ -1,20 +1,25 @@
 import os
 
-# from postgresql_connect import PostgreConnect
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 SECURITY_KEY = os.environ.get('SECURITY_KEY')
-DIALECT = 'postgresql'
-HOST = 'localhost'
-PORT = '5432'
-CHARSET_TYPE = 'utf8mb4'
-db_name = 'postgresql'
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:{SECURITY_KEY}@localhost:5432/mydb"
 
-# This code is DEPLICATED
+# このengineを使用してデータベース操作を行う
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# このクラスを継承して各データベースモデルまたはクラス（ORMモデル）を作成する
+Base = declarative_base()
+
+# *****DUPLICATED*******
+# SECURITY_KEY = os.environ.get('SECURITY_KEY')
+# DIALECT = 'postgresql'
+# HOST = 'localhost'
+# PORT = '5432'
+# CHARSET_TYPE = 'utf8mb4'
+# db_name = 'postgresql'
+#
 # def db_config():
-#     pg = PostgreConnect()
-#     print(f'connected {pg.get_table_list()}')
-
-
-from sqlalchemy import create_engine
-def db_config():
-    return create_engine(f"{DIALECT}://rsato:{SECURITY_KEY}@{HOST}:{PORT}/{db_name}?charset={CHARSET_TYPE}")
+#     return create_engine(f"{DIALECT}://rsato:{SECURITY_KEY}@{HOST}:{PORT}/{db_name}?charset={CHARSET_TYPE}")
